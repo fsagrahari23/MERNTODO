724692f0c52b0aaace0d698a9b7375b5df962e65
fsagrhari <monuagrhari854@gmail.com>
2024-10-07 11:15:35 +0530

    updated

--- Client/src/store/Todo/TodoSlice.js
+++ Client/src/store/Todo/TodoSlice.js
@@ -6,7 +6,7 @@ const initalState={
   isLoading: false,
 }
 const addTodo = createAsyncThunk('/', async (data) => {
-  const res = await axios.post('http://localhost:3500/api/todo/add',data, {
+  const res = await axios.post('https://vercel.com/fsagrahari23s-projects/server/HSELf8VMwRHa5w4ndwVcYrqzYkqz/api/todo/add',data, {
     headers: {
       'Content-Type': 'application/json',
     }, 
@@ -15,7 +15,7 @@ const addTodo = createAsyncThunk('/', async (data) => {
 })
 const editTodo = createAsyncThunk('/', async ({formData, id}) => {
   console.log(id)
-  const res = await axios.put(`http://localhost:3500/api/todo/edit/${id}`,formData, {
+  const res = await axios.put(`https://vercel.com/fsagrahari23s-projects/server/HSELf8VMwRHa5w4ndwVcYrqzYkqz/api/todo/edit/${id}`,formData, {
     headers: {
       'Content-Type': 'application/json',
     }, 
@@ -23,7 +23,7 @@ const editTodo = createAsyncThunk('/', async ({formData, id}) => {
   return res.data;
 })
 const fetchTodo = createAsyncThunk('/', async () => {
-  const res = await axios.get('http://localhost:3500/api/todo/', {
+  const res = await axios.get('https://vercel.com/fsagrahari23s-projects/server/HSELf8VMwRHa5w4ndwVcYrqzYkqz/api/todo/', {
     headers: {
       'Content-Type': 'application/json',
     }, 
@@ -31,7 +31,7 @@ const fetchTodo = createAsyncThunk('/', async () => {
   return res.data;
 })
 const deleteTodo = createAsyncThunk('todo/addTodo', async (id) => {
-  const res = await axios.delete(`http://localhost:3500/api/todo/delete/${id}`, {
+  const res = await axios.delete(`https://vercel.com/fsagrahari23s-projects/server/HSELf8VMwRHa5w4ndwVcYrqzYkqz/api/todo/delete/${id}`, {
     headers: {
       'Content-Type': 'application/json',
     }, 
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3500;
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mern-todo-4brt97viy-fsagrahari23s-projects.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization','Cache-Control'],
